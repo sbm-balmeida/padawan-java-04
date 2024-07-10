1c03387336b0ea9b8e98f798eb892421abbaa762
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-10 13:21:39 -0300

    O método toString()

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -9,9 +9,9 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
+//		System.out.println("x");
+//		System.out.println(3);
+//		System.out.println(false);
 		
 		//Polimorfismo
 		ContaCorrente cc = new ContaCorrente(22, 33);
